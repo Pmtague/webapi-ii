eff0da86598487107708739bbc477627c56cbfe9
Penny Tague <pmtague9@gmail.com>
2019-09-03 16:09:50 -0700

    Getting close

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const data = require('./data/db.js');
+// const express = require('express');
+// const data = require('./data/db.js');
 
-const server = express();
+// const server = express();
 
-server.use(express.json());
+// server.use(express.json());
 
-server.get('/api/posts', (req, res) => {
-	res.status(200).json(posts);
-})
+// server.get('/api/posts', (req, res) => {
+// 	res.status(200).json(posts);
+// })
 
 // server.get('/api/posts/:id', (req, res) => {
-// 	const id = req.params.id;
+	// const id = req.params.id;
 
 // })
